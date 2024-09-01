14aa009fa93c8eac058691f5745f99db48dcf821
Pereira80-dan <dalmeidap80@gmail.com>
2024-09-01 15:35:51 -0300

    subindo

---
+++ src/main/java/com/projeto/livraria/model/entities/Livro.java
@@ -0,0 +1,48 @@
+package com.projeto.livraria.model.entities;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="livros")
+public class Livro {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String titulo;
+    private String autor;
+    private BigDecimal preco;
+
+    public String getTitulo() {
+        return this.titulo;
+       }
+       public String getAutor() {
+        return this.autor;
+       }
+       public BigDecimal getPreco() {
+        return this.preco;
+       }
+       public void setTitulo(String titulo) {
+         this.titulo=titulo;
+       }
+       public void setAutor(String autor) {
+        this.autor=autor;
+      }
+      public void setPreco(BigDecimal preco) {
+        this.preco=preco;
+      }
+}
